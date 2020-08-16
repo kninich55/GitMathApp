c0868df1ea944cc28f3f7aa86660ac98886fe0e6
JOSE-PC\HICHAM <kninich5@aol.com>
2020-08-16 17:08:32 -0400

    implement code for addition by knininch55

--- src/example/MathApp.java
+++ src/example/MathApp.java
@@ -1,10 +1,24 @@
 package example;
 
+import java.util.Scanner;
+
 public class MathApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter first num: ");
+		double num1=scan.nextDouble();
+		System.out.println("Enter second num: ");
+		double num2=scan.nextDouble();
+		
+		System.out.println("-------Operation result-------\n");
+		System.out.println("sum: " + addition(num1,num2));
+		
 
+	}
+
+	private static double addition(double num1, double num2) {
+		return num1+num2;
 	}
 
 }
